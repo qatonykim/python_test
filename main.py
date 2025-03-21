d2993a9da2b2e8fd1e346266fae49cc3e5788d8a
qatonykim <kreatka@gmail.com>
2025-03-21 15:46:29 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 def in_autotests_we_trust(a, b):
     if a == b:
-        print('Test passed')
+        print('Passed')
     else:
-        print('Test failed')
+        print('Failed')
 
 in_autotests_we_trust(10, '10')
 
